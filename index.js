89e0a8f48a68ea6c865e6a7bb4eac6df62e4e857
Daniel Waghorne <dwaghorne@hotmail.com>
2024-01-08 11:38:20 +1000

    lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+        return customerName;
+}
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}   
+
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'someone';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
